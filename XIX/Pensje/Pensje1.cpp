08ad5a0fe4b76a5291674e1d893d5c243151f356
Jan Grzybek <inoklipolos@gmail.com>
2018-03-13 21:50:17 +0100

    Początek debugowania

--- XIX/Pensje/Pensje1.cpp
+++ XIX/Pensje/Pensje1.cpp
@@ -28,6 +28,11 @@ vector<Pracownik> liczpow(vector<Pracownik> wektro)
     }
 }
 
+bool CzySzef(Pracownik p)
+{
+    return p.indexwlasny == p.indexszef;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -53,7 +58,11 @@ int main()
             cin >> Pracownicy[i].pensja;
             Pensje[i] = Pracownicy[i].pensja;
             Pracownicy[i].indexwlasny = i + 1;
-
+            //printf("%d, %d\n", Pracownicy[i].indexwlasny, Pracownicy[i].indexszef);
+            //int y = Pracownicy[i].indexwlasny - Pracownicy[i].indexszef;
+            //printf("y = %d\nsize = %d\n", y, size);
+            //if(y == 0) Pensje[i] = size;
+            if(CzySzef(Pracownicy[i])) Pensje[i] = size;
             Vpracownicy.push_back(Pracownicy[i]);
         }
 
@@ -73,7 +82,7 @@ int main()
         bool *czy = new bool[size]{false}; 
         for(int i = 0, ostatni = 0, flag = 0; i < Vpracownicy.size(); ++i)
         {
-            if(ostatni == Vpracownicy[i].indexszef)
+            if(ostatni == Vpracownicy[i].indexszef && Vpracownicy[i].pensja == 0)
             {
                 czy[i] = true;
                 if(flag == 0) 
@@ -103,7 +112,11 @@ int main()
         {
             if(v[i].pensja == 0)
             {
-                if(Pensje[v[i].indexszef - 1] != 0) Pensje[v[i].indexwlasny - 1] = Pensje[v[i].indexszef - 1]-1;
+                if(Pensje[v[i].indexszef - 1] != 0 && !CzySzef(v[i])) Pensje[v[i].indexwlasny - 1] = Pensje[v[i].indexszef - 1]-1;
+            }
+            else
+            {
+                if(Pensje[v[i].indexszef - 1] == 0) Pensje[v[i].indexszef - 1] = Pensje[v[i].indexwlasny - 1] + 1;
             }
         }
         //printf("\n");
