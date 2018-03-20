0990103e0364e71114ebb72db5a13958e671f6d2
Jan Grzybek <inoklipolos@gmail.com>
2018-03-20 19:40:00 +0100

    Początek

--- XV/Plakatowanie/Plakatowanie.cpp
+++ XV/Plakatowanie/Plakatowanie.cpp
@@ -9,15 +9,12 @@ int ile1(int *wys, int n)
     //for(int i =0 ; i < n; ++i) cout << wys[i] << endl;
     int ile = 0;
     
-    for(int i = 0, c = wys[0]; i < n; ++i)
+    for(int i = 1; i < n; ++i)
     {
-        c = wys[i];
         int k = i;
-        for(; wys[i] == c && i < n; ++i);
+        for(; wys[i] == wys[i - 1] && i < n; ++i);
         if(k < i) ++ile;
-        //c = wys[++i];
     }
-    cout << ile << endl;
     return n - ile;
 }
 
