e423d19885b1d0fc46fca41669c6bbcdb5105361
Sarratas <Sarratas@users.noreply.github.com>
2019-02-07 17:15:09 +0100

    Update gra.cpp

--- XVII/Gra w Minima/gra.cpp
+++ XVII/Gra w Minima/gra.cpp
@@ -8,7 +8,7 @@ int main()
     std::ios_base::sync_with_stdio(0);
     int n = 0;
     std::cin >> n;
-    int *wynik = new int[n];
+    int wynik = 0;
     std::vector<int> karta;
     for (int i = 0, k; i < n; ++i)
     {
@@ -16,11 +16,11 @@ int main()
         karta.push_back(k);
     }
     std::sort(karta.begin(), karta.end());
-    wynik[0] = karta[0];
+    wynik = karta[0];
     for(int i = 1; i < n; ++i)
     {
-        wynik[i] = max(karta[i] - wynik[i - 1], wynik[i - 1]);
+        wynik = max(karta[i] - wynik, wynik);
     }
-    printf("%d\n", wynik[n - 1]);
+    printf("%d\n", wynik);
     return 0;
 }
